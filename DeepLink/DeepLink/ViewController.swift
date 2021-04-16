c113c68f6fe5f860a199940e03542d1db81dd4db
JuliaVorotchenko <juliavorotchenko@gmail.com>
2021-04-16 14:04:13 +0300

    Setup table

--- DeepLink/DeepLink/ViewController.swift
+++ DeepLink/DeepLink/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource
+{
+   
 
+    private let tableView = UITableView()
+    let data  = ["Terms", "Privacy", "Contact"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.view.addSubview(tableView)
+        self.tableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.tableView.frame = view.bounds
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
 }
 
